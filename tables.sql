2aaa670e95e7de4a626556b56b4204456ca7b409
Victoryia Zviahintsava <90476803+vikaaaaa555@users.noreply.github.com>
2023-11-07 03:31:08 +0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,28 +1,29 @@
 CREATE TABLE IF NOT EXISTS role
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	role_type VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	login VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(20) NOT NULL,
 	role_id INT NOT NULL,
 	
 	FOREIGN KEY (role_id) REFERENCES role (id),
-	CHECK (login ~ '^[A-Za-z0-9_]$'),
+	CHECK (login ~ '^[A-Za-z0-9_]+$'),
 	CHECK (password ~ '^[a-zA-Z0-9\-_@%]+$')
 );
 
 CREATE TABLE IF NOT EXISTS profile
 (
-	user_id SERIAL PRIMARY KEY,
+	user_id SMALLINT PRIMARY KEY,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
 	email VARCHAR(30) NOT NULL,
 	phone_number VARCHAR(20),
+	birth_date DATE,
 	
 	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
 	CHECK (first_name ~ '^[a-zA-Z]+$'),
@@ -33,7 +34,7 @@ CREATE TABLE IF NOT EXISTS profile
 
 CREATE TABLE IF NOT EXISTS action_type
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	name VARCHAR(30) NOT NULL UNIQUE,
 	
 	CHECK (name ~ '^[a-zA-Z]+$')
@@ -41,7 +42,7 @@ CREATE TABLE IF NOT EXISTS action_type
 
 CREATE TABLE IF NOT EXISTS action
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	action_time TIMESTAMP NOT NULL,
 	user_id INT,
 	action_type_id INT NOT NULL,
@@ -52,15 +53,15 @@ CREATE TABLE IF NOT EXISTS action
 
 CREATE TABLE IF NOT EXISTS doc_type
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	name VARCHAR(30) NOT NULL UNIQUE,
 	
-	CHECK (name ~ '^[a-zA-Z]+$')
+	CHECK (name ~ '^[a-zA-Z ]+$')
 );
 
 CREATE TABLE IF NOT EXISTS document
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	title VARCHAR(50) NOT NULL,
 	description TEXT NOT NULL,
 	last_change_time TIMESTAMP NOT NULL,
@@ -71,7 +72,7 @@ CREATE TABLE IF NOT EXISTS document
 
 CREATE TABLE IF NOT EXISTS state_system
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	system_type VARCHAR(30) NOT NULL UNIQUE,
 	description TEXT,
 	
@@ -80,33 +81,33 @@ CREATE TABLE IF NOT EXISTS state_system
 
 CREATE TABLE IF NOT EXISTS countrie
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	name VARCHAR(40) NOT NULL UNIQUE,
 	capital VARCHAR(30) NOT NULL,
 	state_system_id INT NOT NULL,
 	
 	FOREIGN KEY (state_system_id) REFERENCES state_system (id),
-	CHECK (name ~ '^[a-zA-Z]+$'),
-	CHECK (capital ~ '^[a-zA-Z]+$')
+	CHECK (name ~ '^[a-zA-Z ]+$'),
+	CHECK (capital ~ '^[a-zA-Z ]+$')
 );
 
 CREATE TABLE IF NOT EXISTS historical_figure
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	first_name VARCHAR(45) NOT NULL,
 	last_name VARCHAR(45) NOT NULL,
 	birth_date DATE NOT NULL,
 	death_date DATE,
 	
 	CHECK (first_name ~ '^[a-zA-Z]+$'),
-	CHECK (last_name ~ '^[a-zA-Z]+$')
+	CHECK (last_name ~ '^[a-zA-Z ]+$')
 );
 
 CREATE TABLE IF NOT EXISTS figure_countrie_doc_linc
 (
-	historical_figure_id INT NOT NULL,
-	countrie_id INT NOT NULL,
-	document_id INT NOT NULL,
+	historical_figure_id SMALLINT NOT NULL,
+	countrie_id SMALLINT NOT NULL,
+	document_id SMALLINT NOT NULL,
 	
 	PRIMARY KEY (historical_figure_id, countrie_id, document_id),
 	FOREIGN KEY (historical_figure_id) REFERENCES historical_figure (id),
@@ -116,15 +117,15 @@ CREATE TABLE IF NOT EXISTS figure_countrie_doc_linc
 
 CREATE TABLE IF NOT EXISTS collection
 (
-	id SERIAL PRIMARY KEY,
+	id SMALLINT PRIMARY KEY UNIQUE,
 	name VARCHAR(40) NOT NULL,
 	description TEXT
 );
 
 CREATE TABLE IF NOT EXISTS user_collection_link
 (
-	user_id INT NOT NULL,
-	collection_id INT NOT NULL,
+	user_id SMALLINT NOT NULL,
+	collection_id SMALLINT NOT NULL,
 	is_subscribe BOOLEAN,
 	
 	PRIMARY KEY (user_id, collection_id),
@@ -134,9 +135,9 @@ CREATE TABLE IF NOT EXISTS user_collection_link
 
 CREATE TABLE IF NOT EXISTS collection_doc_link
 (
-	collection_id INT NOT NULL,
-	document_id INT NOT NULL,
+	collection_id SMALLINT NOT NULL,
+	document_id SMALLINT NOT NULL,
 	
 	FOREIGN KEY (collection_id) REFERENCES collection (id),
-	FOREIGN KEY (document_id) REFERENCES collection (id) ON DELETE CASCADE
+	FOREIGN KEY (document_id) REFERENCES document (id) ON DELETE CASCADE
 );
